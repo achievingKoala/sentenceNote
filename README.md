5ef281604cba7ed96ced4bc7fe60bf1262c529c9
lkw <zgbtlkw@gmail.com>
2025-09-27 21:59:25 +0800

    change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# poem-app
+# sentence-app
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/poem-app/',
+  base: '/sentence-app/',
 })
