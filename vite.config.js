a2bf0f255e2639f43f66970d46f4d4e6f9cc2f9f
lkw <zgbtlkw@gmail.com>
2025-10-16 19:35:56 +0800

    change url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/sentenceNote/',
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
